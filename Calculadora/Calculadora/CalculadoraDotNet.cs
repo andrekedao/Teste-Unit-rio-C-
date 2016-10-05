fe92e0c7a0698bc556c0d1aa09d18b807020488f
andre <andrekedao27@gmail.com>
2016-10-04 23:37:30 -0300

    Segundo commit com classe de teste usando UnitTest

--- Calculadora/Calculadora/CalculadoraDotNet.cs
+++ Calculadora/Calculadora/CalculadoraDotNet.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Calculadora
 {
-    class CalculadoraDotNet
+    public class CalculadoraDotNet
     {
         static void Main(string[] args)
         {
---
+++ Calculadora/Testes/testaCalculadora.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculadora;
+
+namespace Testes
+{
+    [TestClass]
+    public class TestaCalculadora
+    {
+        [TestMethod]
+        public void TestSoma()
+        {
+            CalculadoraDotNet c = new CalculadoraDotNet();
+            int resultado = c.Soma(3, 4);
+            Assert.AreEqual(7, resultado);
+        }
+    }
+}
